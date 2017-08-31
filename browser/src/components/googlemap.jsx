b30e4041e233eb22220f2a8727c88ae6b9399b60
Lily Vang <lily.sf888@gmail.com>
2017-08-30 19:35:13 -0700

    setting lat and lng in state

--- browser/src/components/googlemap.jsx
+++ browser/src/components/googlemap.jsx
@@ -1,25 +1,7 @@
 import React, { Component } from 'react';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-// get user's current position with HTML Geolocation
-// let userLat;
-// let userLng;
-
-// const showPosition = position => {
-// 	userLat = position.coords.latitude;
-// 	userLng = position.coords.longitude;
-// 	console.log(userLat, userLng);
-// };
-
-// const getLocation = () => {
-// 	if (navigator.geolocation) {
-// 		navigator.geolocation.getCurrentPosition(showPosition);
-// 	} else {
-// 		console.log('Geolocation is not supported by this browser');
-// 	}
-// };
-
-export class MapContainer extends React.Component {
+export class MapContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -53,24 +35,22 @@ export class MapContainer extends React.Component {
 		const userLat = this.state.userLat;
 		const userLng = this.state.userLng;
 
-		return {
-			userLat: userLng ? (
-				<Map
-					google={this.props.google}
-					style={{ height: '50%', width: '50%' }}
-					zoom={8}
-					initialCenter={{
-						lat: { userLat },
-						lng: { userLng }
-					}}
-				>
-					<Marker name={'current location'} position={{ lat: 37.778519, lng: -122.40564 }} />
-					<Marker name={'Dolores park'} position={{ lat: 37.759703, lng: -122.428093 }} />
-				</Map>
-			) : (
-				<div>Loading...</div>
-			)
-		};
+		return (
+			<div>
+				{userLat && (
+					<Map
+						google={this.props.google}
+						zoom={14}
+						initialCenter={{
+							lat: userLat,
+							lng: userLng
+						}}
+					>
+						<Marker onClick={this.onMarkerClick} name={'asdf'} />
+					</Map>
+				)}
+			</div>
+		);
 	}
 }
 
