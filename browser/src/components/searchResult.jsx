d968398c3f53296907e2f78e5423847a46380aa2
Lily Vang <lily.sf888@gmail.com>
2017-10-05 16:16:56 -0700

    bring in icons

--- browser/src/components/searchResult.jsx
+++ browser/src/components/searchResult.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import FaStarO from 'react-icons/lib/fa/star-o';
+import FaBuilding from 'react-icons/lib/fa/building';
+import MdLocationOn from 'react-icons/lib/md/location-on';
+import FaGlobe from 'react-icons/lib/fa/globe';
 import Services from '../services/venues';
-
 
 const SearchResult = ({ venues }) => {
 	const venue = venues.map((item, index) =>
 		<table>
-			<tr><td key={index}>{item.venue.name}</td></tr>
-			<tr><td key={index}>{item.venue.location.address}, {item.venue.location.city}</td></tr>
-			<tr><td key={index}>{item.venue.rating}</td></tr>
-			<tr><td key={index}>{item.venue.stats.checkinsCount}</td></tr>
-			<tr><td key={index}>{item.venue.url}</td></tr>
+			<tr><th key={index}>{item.venue.name}</th></tr>
+			<tr>
+				<td><FaBuilding /></td>
+				<td key={index}>{item.venue.location.address}, {item.venue.location.city}</td>
+			</tr>
+			<tr>
+				<td><FaStarO /></td>
+				<td key={index}>{item.venue.rating} /10 ratings</td>
+			</tr>
+			<tr>
+				<td><MdLocationOn /></td>
+				<td key={index}>{item.venue.stats.checkinsCount} check-ins</td>
+			</tr>
+			<tr>
+				<td><FaGlobe /></td>
+				<td key={index}>{item.venue.url}</td>
+			</tr>
 		</table>
 	);
 
